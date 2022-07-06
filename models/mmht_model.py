74f6f1f746f4462ed861c6f5fc0155074c033bc7
zhenghui <wzhdx123@126.com>
2022-07-06 22:04:31 +0800

    clip mask revise

--- models/mmht_model.py
+++ models/mmht_model.py
@@ -39,7 +39,14 @@ class mmhtModel(BaseModel):
             self.criterionL1 = torch.nn.L1Loss()
             self.criterionL2 = torch.nn.MSELoss()
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
-            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            clip_params = list(map(id, self.netG.clip_model.parameters()))
+            base_params = filter(lambda p: id(p) not in clip_params, self.netG.parameters())
+            # self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_G = torch.optim.Adam([
+                {'params': base_params},
+                {'params': self.netG.clip_model.parameters(), 'lr': opt.lr * 0.00001},
+            ], lr=opt.lr, betas=(opt.beta1, 0.999))
+            # self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizers.append(self.optimizer_G)
 
     def set_position(self, pos, patch_pos=None):
